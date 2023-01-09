31796e7c196f8a46f4d2624ec3f694e8bd772036
hyungeun <kingwang09@gmail.com>
2023-01-09 20:57:47 +0900

    live url 설정 추가

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
-const mainUrl = "http://localhost:8080";
+//const mainUrl = "http://localhost:8080";
+const mainUrl = "https://port-0-daniel-spring-boot-9z2ygb26lc9xjovs.gksl2.cloudtype.app";
 const config = {
     "elastic": {
         "url": mainUrl+"/elastic/all",
